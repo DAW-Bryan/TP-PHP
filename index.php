40e54a6f92048c0fbebfa2fbb37a5fa367f36090
ibryans <bryans0701@gmail.com>
2018-07-28 13:31:26 -0300

    Alterando pagina principal

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+  <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Reservas Coltec</title>
+
+        <!-- Bulma CSS -->
+        <link rel="stylesheet" href="./css/bulma.min.css">
+
+        <!-- Font Awesome -->
+        <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
+
+        <!-- JQuery -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  </head>
+
+  <body>
+
+        <!-- Menu navbar -->
+        <?php include "Includes/menu.inc"; ?>
+
+        <section class="section">
+            <div class="container">
+                <h1 class="title"> Reserva de espaços Coltec</h1>
+            </div>
+        </section>
+  </body>
+
+
+  <script>
+        $(document).ready(function() {
+            $(".navbar-burger").click(function() {
+                $(".navbar-burger").toggleClass("is-active");
+                $(".navbar-menu").toggleClass("is-active");
+            });
+        });
+  </script>
+</html>
